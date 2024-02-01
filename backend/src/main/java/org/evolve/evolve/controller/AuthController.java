d141ae756dca5bfb2fd83cf215ba880cc7d182ba
David Rabko <david.rabko@icloud.com>
2024-02-01 15:17:48 +0100

    Start sign up / in
    Co-authored-by: Elovard <persall2013@gmail.com>

---
+++ backend/src/main/java/org/evolve/evolve/controller/AuthController.java
@@ -0,0 +1,34 @@
+package org.evolve.evolve.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.evolve.evolve.dto.JwtAuthenticationResponse;
+import org.evolve.evolve.dto.SignInRequest;
+import org.evolve.evolve.dto.SignUpRequest;
+import org.evolve.evolve.service.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Аутентификация")
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @Operation(summary = "Регистрация пользователя")
+    @PostMapping("/sign-up")
+    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        return authenticationService.signUp(request);
+    }
+
+    @Operation(summary = "Авторизация пользователя")
+    @PostMapping("/sign-in")
+    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return authenticationService.signIn(request);
+    }
+}
--- backend/src/main/java/org/evolve/evolve/service/UserService.java
+++ backend/src/main/java/org/evolve/evolve/service/UserService.java
@@ -1,8 +1,91 @@
 package org.evolve.evolve.service;
 
+import lombok.RequiredArgsConstructor;
+import org.evolve.evolve.entity.Role;
 import org.evolve.evolve.entity.User;
+import org.evolve.evolve.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public interface UserService extends UserDetailsService {
-    public User findByUserName(String userName);
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository repository;
+
+    /**
+     * Сохранение пользователя
+     *
+     * @return сохраненный пользователь
+     */
+    public User save(User user) {
+        return repository.save(user);
+    }
+
+    /**
+     * Создание пользователя
+     *
+     * @return созданный пользователь
+     */
+    public User create(User user) {
+        if (repository.existsByUsername(user.getUsername())) {
+            // Заменить на свои исключения
+            throw new RuntimeException("Пользователь с таким именем уже существует");
+        }
+
+        if (repository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Пользователь с таким email уже существует");
+        }
+
+        return save(user);
+    }
+
+    public User getByUsername(String userName) {
+        return repository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException("User is not found"));
+    }
+
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = getByUsername(userName);
+        return new User(user.getUsername(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.getRole());
+    }
+
+    /**
+     * Получение пользователя по имени пользователя
+     * <p>
+     * Нужен для Spring Security
+     *
+     * @return пользователь
+     */
+    public UserDetailsService userDetailsService() {
+        return this::getByUsername;
+    }
+
+    /**
+     * Получение текущего пользователя
+     *
+     * @return текущий пользователь
+     */
+    public User getCurrentUser() {
+        // Получение имени пользователя из контекста Spring Security
+        var username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getByUsername(username);
+    }
+
+    /**
+     * Выдача прав администратора текущему пользователю
+     * <p>
+     * Нужен для демонстрации
+     */
+    @Deprecated
+    public void getAdmin() {
+        var user = getCurrentUser();
+        user.setRole(Role.ROLE_ADMIN);
+        save(user);
+    }
 }
