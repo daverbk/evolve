8af97dac0fc2f36c3728ba3db67c7c93a8d75712
David Rabko <david.rabko@icloud.com>
2024-03-07 14:05:55 +0100

    Add sign-up test

--- backend/src/main/java/org/evolve/dto/request/SignUpRequest.java
+++ backend/src/main/java/org/evolve/dto/request/SignUpRequest.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Schema(description = "Sign up request")
 public class SignUpRequest {
 
